a8bf954422cdc5c8866d942e948093b417b6be0a
LAVANYA CHADARAM <120081620+lavanyachadaram@users.noreply.github.com>
2023-09-19 09:37:05 +0530

    #include<stdio.h> #include<limits.h> int main() {     int t,c=0;     scanf("%d",&t);     while(t--)     {         int n;         scanf("%d",&n);         int arr[n];         for(int i=0;i<n;i++) {             scanf("%d",&arr[i]);             }             for(int k=1;k<n;k++) {                 if(arr[k-1]>arr[k])                 {                     c++;                     }                     }                     if(c==0)                     {                         printf("%d",c);
                        } else 
                        {
                            int min=INT_MAX,max=INT_MIN;
                            for(int l=0;l<n;l++)
                            {
                                if(min>arr[l]) 
                                {
                                    min=arr[l];
                                    }
                                    if(max<arr[l])
                                    {
                                        max=arr[l];
                                        }
                                        }
                                        printf("%d
    ",max-min);
                                        }
                                        }
                                        }

--- Optimal_Sorting.c
+++ Optimal_Sorting.c
@@ -1,50 +1,43 @@
 #include<stdio.h>
 #include<limits.h>
 int main()
-{
-    int t,c=0;
+{ 
+    int t,c=0; 
     scanf("%d",&t);
-    while(t--)
+    while(t--) 
     {
         int n;
-        scanf("%d",&n); 
-        int arr[n];
-        for(int i=0;i<n;i++) 
-        {
+        scanf("%d",&n);
+        int arr[n]; 
+        for(int i=0;i<n;i++) { 
             scanf("%d",&arr[i]);
             }
-            for(int k=1;k<n;k++)
-            {
+            for(int k=1;k<n;k++) { 
                 if(arr[k-1]>arr[k])
                 {
-                    c++; 
-                    
-                }
-                }
-                if(c==0) 
-                {
-                    printf("%d",c);
+                    c++;
                     }
-                    else
+                    }
+                    if(c==0)
+                    {
+                        printf("%d",c); 
+                        
+                    } else 
                     {
                         int min=INT_MAX,max=INT_MIN;
                         for(int l=0;l<n;l++)
                         {
-                            if(min>arr[l])
+                            if(min>arr[l]) 
                             {
-                                min=arr[l]; 
-                                
-                            }
-                            if(max<arr[l]) 
-                            {
-                                max=arr[l];
+                                min=arr[l];
                                 }
-                                }
-                                printf("%d
-",max-min); 
-                        
-                    } 
-        
-    }
-    
-}
+                                if(max<arr[l])
+                                {
+                                    max=arr[l];
+                                    }
+                                    }
+                                    printf("%d
+",max-min);
+                                    }
+                                    }
+                                    }
