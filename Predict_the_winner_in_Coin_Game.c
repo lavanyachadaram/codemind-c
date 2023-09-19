599169918190115167ed9737c607658595dd1fd5
LAVANYA CHADARAM <120081620+lavanyachadaram@users.noreply.github.com>
2023-09-19 12:27:28 +0530

    #include<stdio.h> int main() {     int n,m; scanf("%d%d",&n,&m); if(n%2==0||m%2==0) {     printf("Player 1"); } else {     printf("Player 2"); }
    }

--- Predict_the_winner_in_Coin_Game.c
+++ Predict_the_winner_in_Coin_Game.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
-{ int m,n;
-scanf("%d%d",&m,&n);
-if(m%2==0||n%2==0)
+{
+    int n,m;
+scanf("%d%d",&n,&m);
+if(n%2==0||m%2==0)
 {
     printf("Player 1");
-    }
-    else 
-    {
-        printf("Player 2"); 
-        
-    }
+}
+else
+{
+    printf("Player 2");
+}
     
 }
