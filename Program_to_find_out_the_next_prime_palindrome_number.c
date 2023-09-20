4387a2428a3e3080ccad96b7a87346aafe1fb79a
LAVANYA CHADARAM <120081620+lavanyachadaram@users.noreply.github.com>
2023-09-20 13:21:11 +0530

    #include<stdio.h> int prime(int); int pal(int); int main() {     int n,i; scanf("%d",&n); for(i=n+1;;i++) {     if(prime(i)&&pal(i)) {     printf("%d",i); break; } }
    }
    int prime(int n)
    {
        int f=0;
    for(int j=1;j<=n;j++)
    {
        if(n%j==0)
    {
        f++;
    }
    }
    if(f==2)
    {
        return 1;
    }else{
        return 0;
    }
        
    }
    int pal(int s)
    {
        int r,rev=0,t;
    t=s;
    while(s!=0)
    {
        r=s%10;
    rev=rev*10+r;
    s=s/10;
    }
    if(rev==t)
    {
        return 1;
    }else{
        return 0;
    }
        
    }

--- Program_to_find_out_the_next_prime_palindrome_number.c
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -4,54 +4,50 @@ int pal(int);
 int main()
 {
     int n,i;
-    scanf("%d",&n); 
-    for(i=n+1;;i++)    
-    {
-        if(prime(i)&&pal(i))
-        {
-            printf("%d",i);
-            break; 
-            }
-            }
+scanf("%d",&n);
+for(i=n+1;;i++)
+{
+    if(prime(i)&&pal(i))
+{
+    printf("%d",i);
+break;
+}
+}
     
 }
 int prime(int n)
 {
-    int f=0;    
-    for(int j=1;j<=n;j++) 
-    {
-        if(n%j==0)
-        {
-            f++;
-            }
-            }
-            if(f==2)    
-            {
-                return 1;
-                }
-                else{
-                    return 0;  
-                    }
-    
-
+    int f=0;
+for(int j=1;j<=n;j++)
+{
+    if(n%j==0)
+{
+    f++;
+}
+}
+if(f==2)
+{
+    return 1;
+}else{
+    return 0;
+}
     
 }
-int pal(int s){
+int pal(int s)
+{
     int r,rev=0,t;
-    t=s;    
-    while(s!=0)
-    {
-        r=s%10;
-        rev=rev*10+r;        
-        s=s/10;
-        }
-        if(rev==t) 
-        {
-            return 1;   
-            }
-            else    
-            {
-                return 0;  
-                }
+t=s;
+while(s!=0)
+{
+    r=s%10;
+rev=rev*10+r;
+s=s/10;
+}
+if(rev==t)
+{
+    return 1;
+}else{
+    return 0;
+}
     
 }
